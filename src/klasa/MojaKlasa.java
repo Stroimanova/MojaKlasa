8480e6d55400f778ccb308ec0c4a655eefbc8957
unknown <simonastroimanova@hotmail.com>
2024-02-11 22:52:09 +0100

    add first java program

---
+++ src/klasa/MojaKlasa.java
@@ -0,0 +1,8 @@
+package klasa;
+import java.util.Scanner;
+public class MojaKlasa {
+	public static void main(String[] args) {
+		Scanner myVar=new Scanner(System.in);
+		System.out.print(myVar.nextLine());
+		}
+}
